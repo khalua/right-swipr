f35ca0834f539ffd2df5b47957b1ab19916df475
Tony Contreras <tony.contreras@gmail.com>
2013-03-13 16:23:48 -0400

    logins work

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 R20130312RightSwipr::Application.routes.draw do
   root :to => 'welcome#index'
-
   resources :subscribers, :only => [:new, :create]
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+
 end
---
+++ spec/features/session_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe 'Session' do
+  describe 'GET /' do
+    it 'displays a login link' do
+      visit root_path
+      page.should have_link('Login')
+    end
+  end
+
+  describe 'GET /login' do
+    it 'displays the login form', :js => true do
+      visit root_path
+      click_link('Login')
+      page.should have_button('Cancel')
+      page.should have_button('Start right swipin')
+    end
+  end
+
+describe 'POST /login' do
+  let(:user) {User.create(email: 'bob@gmail.com', username: 'Bob', password: 'a', password_confirmation: 'a')}
+
+  it 'logs the user into the system if credentials are correct', :js => true do
+    visit root_path
+    click_link('Login')
+    fill_in('Email', :with => user.email)
+    fill_in('Password', :with => 'a')
+    click_button('Start right swipin')
+    page.should_not have_button('Start right swipin')
+  end
+
+  it 'logs the user into the system if credentials are incorrect', :js => true do
+    visit root_path
+    click_link('Login')
+    fill_in('Email', :with => user.email)
+    fill_in('Password', :with => 'b')
+    click_button('Start right swipin')
+    page.should have_button('Start right swipin')
+  end
+end
+
+  describe 'JS cancel_login_form()' do
+    it 'removes the login form', :js => true do
+      visit root_path
+      click_link('Login')
+      click_button('Cancel')
+      page.should_not have_button('Start right swipin')
+    end
+  end
+
+
+end
